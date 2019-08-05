11f68518374183f4f969fc36669a25531323d4ba
ctschubel <luenidev@gmx.de>
2019-08-05 20:08:18 +0200

    CTS: fixed database connection

--- src/main/java/de/luenidev/shop/CustomerService/model/Customer.java
+++ src/main/java/de/luenidev/shop/CustomerService/model/Customer.java
@@ -1,12 +1,20 @@
 package de.luenidev.shop.CustomerService.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Simple customer Object.
  *
  * @author ctschubel
  */
+@Entity
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     private String email;
